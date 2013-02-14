3f3303da3766c227ee45157459ffc6161e343b1c
Ryan <ryan.joshua.jones@gmail.com>
2013-02-13 17:56:24 -0800

    Added routing/handling of POST, now displayed in /upload page

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response) {
     console.log("Request handler 'start' was called");
@@ -22,11 +22,11 @@ function start(response) {
 }
 
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called");
-    
+
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello upload");
+    response.write("You've sent " + querystring.parse(postData).text);
     response.end();
 }
 
